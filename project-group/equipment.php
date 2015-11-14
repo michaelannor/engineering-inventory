8522416e02c3160511ac7a14e88c105539e2e683
Nuralight <nuraabd4@gmail.com>
2015-11-14 15:55:23 +0000

    equipment Class

--- project-group/equipment.php
+++ project-group/equipment.php
@@ -1,33 +1,19 @@
 <?php
-include_once("adb.php");
-class equipment extends adb{
-function add_equipment($name,$time_purchased,$description,$price){
-$str_query="insert into equipment set equipments_name='$name',time_purchased='$time_purchased', description='$description', price=$price";
-return $this->query($str_query).mysql_error();
+	/**
+	*@author Nura Abdul-Rahman <nuraabd4@gmail.com>
+	*/
+	include_once("adb.php");
+	class equipment extends adb{
+	/**
+	*@method boolean add_equipment($name,$time_purchased,$description,$price) adds equipment to the equipment table
+	*@param string $name name of equipment
+	*@param integer $time_purchase time equipment was purchased
+	*@param string $description  description of equipment
+	*@param integer $price price of equipment
+    	**/
+	function add_equipment($name,$time_purchased,$description,$price){
+	 $str_query="INSERT INTO equipment set equipments_name='$name',time_purchased='$time_purchased', description='$description', price=$price";
+	 return $this->query($str_query).mysql_error();
+	}
 
-}
-function delete_equipment($id){
-	$str_query="delete from equipment where equipments_id=$id";
-	return $this->query($str_query);
-}
-function view_equipment(){
-	$str_query="select equipments_id, equipments_name,time_purchased,description, price from equipment";
-	return $this->query($str_query);
-}
-function search_equipment($search_text){
-	$str_query="select equipments_id,equipments_name from equipment where equipments_name like'%$search_text%'";
-	return $this->query($str_query);
-}
-function view($id){
-	$str_query="select  equipments_id ,equipments_name,time_purchased,description, price from equipment where equipments_id=$id";
-	return $this->query($str_query);
-}
-function edit_equipment($name,$time_purchased,$description,$price,$id){
-	$str_query="update equipment set equipments_name='$name', time_purchased='$time_purchased', description='$description', price='$price'
-	where equipments_id=$id";
-	return $this->query($str_query);
-}
-}
 ?>
-
-
