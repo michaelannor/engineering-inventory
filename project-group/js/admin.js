c2f12951c8c31640514e72ee3fb71912252c319e
michaelannor <m.annor13@gmail.com>
2015-11-15 21:16:13 +0000

    got the hover effect towork dynamically after... so long...since the last commit

--- project-group/js/admin.js
+++ project-group/js/admin.js
@@ -14,27 +14,24 @@ function viewHome() {
 
   content +=  "<div class='col-md-3'><a href='#'>";
 
-        content += "<img onmouseover='hover(\'../media/co2.png\',\'co-img\');' onmouseout='unhover(\'../media/co.png\',\'co-img\');' src='../media/co.png' class='img-responsive' id='co-img'>";
+        // content += "<img onmouseover='hover(\x27../media/co2.png\x27,\x27co-img\x27);' onmouseout='unhover(\x27../media/co.png\x27,\x27co-img\x27);' src='../media/co.png' class='img-responsive' id='co-img'>";
       content += "</a><span class='wrapper'><hr></span></div>";
 
       content +=  "<div class='col-md-3'><a href='#'>";
 
-        content += "<img onmouseover='hover(\'../media/ci2.png\',\'ci-img\');' onmouseout='unhover(\'../media/ci.png\',\'ci-img\');' src='../media/ci.png' class='img-responsive' id='ci-img'>";
+        // content += "<img onmouseover='hover(\'../media/ci2.png\',\'ci-img\');' onmouseout='unhover(\'../media/ci.png\',\'ci-img\');' src='../media/ci.png' class='img-responsive' id='ci-img'>";
         content += "</a><span class='wrapper'><hr></span></div>";
 
 
         content +=  "<div class='col-md-3'><a href='#'>";
 
-        content += "<img onmouseover='hover(\'../media/ad2.png\',\'ad-img\');' onmouseout='unhover(\'../media/ad.png\',\'ad-img\');' src='../media/ad.png' class='img-responsive' id='ad-img'>";
+        // content += "<img onmouseover='hover(\'../media/ad2.png\',\'ad-img\');' onmouseout='unhover(\'../media/ad.png\',\'ad-img\');' src='../media/ad.png' class='img-responsive' id='ad-img'>";
         content += "</a><span class='wrapper'><hr></span></div>";
 
         content +=  "<div class='col-md-3'><a href='#'>";
 
-      content += "<a href=''><img onmouseover='hover(\'../media/la2.png\',\'la-img\');' onmouseout='unhover(\'../media/la.png\',\'la-img\');' src='../media/la.png' class='img-responsive' id='la-img'></a>";
+      content += "<img src='../media/la.png' class='img-responsive' id='la-img'>";
       content += "</a><span class='wrapper'><hr></span></div>";
-
-
-
 
   $("#main-content").html(content);
 }
@@ -49,4 +46,16 @@ $(function(){
     view_check_out_equipment();
   });
 
+});
+
+$(function() {
+    $("img")
+        .mouseover(function() {
+            // var src = $(this).attr("src").match(/[^\.]+/) + "2.png";
+            $(this).attr("src", "../media/la2.png");
+        })
+        .mouseout(function() {
+            // var src = $(this).attr("src").replace("../media/la.png", ".png");
+            $(this).attr("src", "../media/la.png");
+        });
 });
