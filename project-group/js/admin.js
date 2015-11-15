84ac09e8060388d13a0cabdd0e52a891f45e4e77
michaelannor <m.annor13@gmail.com>
2015-11-15 22:32:57 +0000

    dynamically loads checkout form

--- project-group/js/admin.js
+++ project-group/js/admin.js
@@ -13,43 +13,59 @@ function viewHome() {
   var content = "";
 
   content +=  "<div class='col-md-3'><a href='#'>";
-
-        // content += "<img onmouseover='hover(\x27../media/co2.png\x27,\x27co-img\x27);' onmouseout='unhover(\x27../media/co.png\x27,\x27co-img\x27);' src='../media/co.png' class='img-responsive' id='co-img'>";
-      content += "</a><span class='wrapper'><hr></span></div>";
-
-      content +=  "<div class='col-md-3'><a href='#'>";
-
-        // content += "<img onmouseover='hover(\'../media/ci2.png\',\'ci-img\');' onmouseout='unhover(\'../media/ci.png\',\'ci-img\');' src='../media/ci.png' class='img-responsive' id='ci-img'>";
-        content += "</a><span class='wrapper'><hr></span></div>";
-
-
-        content +=  "<div class='col-md-3'><a href='#'>";
-
-        // content += "<img onmouseover='hover(\'../media/ad2.png\',\'ad-img\');' onmouseout='unhover(\'../media/ad.png\',\'ad-img\');' src='../media/ad.png' class='img-responsive' id='ad-img'>";
-        content += "</a><span class='wrapper'><hr></span></div>";
-
-        content +=  "<div class='col-md-3'><a href='#'>";
-
-      content += "<img src='../media/la.png' class='img-responsive' id='la-img'>";
-      content += "</a><span class='wrapper'><hr></span></div>";
+  content += "<img src='../media/co.png' class='img-responsive' id='co-img'>";
+  content += "</a><span class='wrapper'><hr></span></div>";
+  content +=  "<div class='col-md-3'><a href='#'>";
+  content += "<img src='../media/ci.png' class='img-responsive' id='ci-img'>";
+  content += "</a><span class='wrapper'><hr></span></div>";
+  content +=  "<div class='col-md-3'><a href='#'>";
+  content += "<img src='../media/ad.png' class='img-responsive' id='ad-img'>";
+  content += "</a><span class='wrapper'><hr></span></div>";
+  content +=  "<div class='col-md-3'><a href='#'>";
+  content += "<img src='../media/la.png' class='img-responsive' id='la-img'>";
+  content += "</a><span class='wrapper'><hr></span></div>";
 
   $("#main-content").html(content);
 }
 
 function view_check_out_equipment() {
-  // body...
+  var content = "";
+
+  content += "<form class='form col-md-12 center-block'><div class='form-group'><label for='checkout_equipment_id_input'>Equipment ID</label>";
+  content += "<input id='checkout_equipment_id_input' type='text' class='form-control input-lg' placeholder='Equipment ID'></div><div class='form-group'>";
+  content += "<label for='checkout_student_id_input'>Student ID</label><input id='checkout_student_id_input' type='text' class='form-control input-lg' placeholder='Student ID'></div>";
+  content += "<div class='form-group'><label for='checkout_dateout_id_input'>Date Checked Out:</label><input id='checkout_dateout_id_input' type='date' class='form-control input-lg' ";
+  content += "placeholder='DD/MM/YYYY'></div><div class='form-group'><label for='checkout_datein_id_input'>Return Date:</label><input id='checkout_datein_id_input' type='date' ";
+  content += "class='form-control input-lg' placeholder='DD/MM/YYYY'>";
+  content += "</div><div class='form-group'><button id='checkoutbtn' class='btn btn-primary btn-lg btn-block'>Check Out</button></div><br><hr></form>";
+
+  $("#main-content").html(content);
+
+}
+
+function check_out_equipment() {
+  var equipment = $("#checkout_equipment_id_input");
+  var student = $("#checkout_student_id_input");
+  var borrowed = $("#checkout_dateout_id_input");
+  var datereturn = $("#checkout_datein_id_input");
 }
 
 $(function(){
 
-  $("#check_out_equipment_btn").click(function(){
+  $("#la-img").click(function(){
     view_check_out_equipment();
   });
+
+  $("#checkoutbtn").click(function(){
+    check_out_equipment();
+  });
+
+
 
 });
 
 $(function() {
-    $("img")
+    $("#la-img")
         .mouseover(function() {
             // var src = $(this).attr("src").match(/[^\.]+/) + "2.png";
             $(this).attr("src", "../media/la2.png");
@@ -57,5 +73,41 @@ $(function() {
         .mouseout(function() {
             // var src = $(this).attr("src").replace("../media/la.png", ".png");
             $(this).attr("src", "../media/la.png");
+        });
+});
+
+$(function() {
+    $("#ad-img")
+        .mouseover(function() {
+            // var src = $(this).attr("src").match(/[^\.]+/) + "2.png";
+            $(this).attr("src", "../media/ad2.png");
+        })
+        .mouseout(function() {
+            // var src = $(this).attr("src").replace("../media/la.png", ".png");
+            $(this).attr("src", "../media/ad.png");
+        });
+});
+
+$(function() {
+    $("#ci-img")
+        .mouseover(function() {
+            // var src = $(this).attr("src").match(/[^\.]+/) + "2.png";
+            $(this).attr("src", "../media/ci2.png");
+        })
+        .mouseout(function() {
+            // var src = $(this).attr("src").replace("../media/la.png", ".png");
+            $(this).attr("src", "../media/ci.png");
+        });
+});
+
+$(function() {
+    $("#co-img")
+        .mouseover(function() {
+            // var src = $(this).attr("src").match(/[^\.]+/) + "2.png";
+            $(this).attr("src", "../media/co2.png");
+        })
+        .mouseout(function() {
+            // var src = $(this).attr("src").replace("../media/la.png", ".png");
+            $(this).attr("src", "../media/co.png");
         });
 });
