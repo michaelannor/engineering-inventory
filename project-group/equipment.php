926096b3bb05bfd0b60b0fc6412fb87f466e94c4
Nuralight <nuraabd4@gmail.com>
2015-11-22 10:21:03 +0000

    equipment
    new function added (faults log) to equipment class

--- project-group/equipment.php
+++ project-group/equipment.php
@@ -13,9 +13,21 @@
 	*@param integer $lab_id lab  where equipment belong
 	*@param string $safety_requirement the safety requirement of equipment
     	**/
-	function add_equipment($name,$id,$manufacturer,$supplier,$lab_id,$safety_requirement){
-	 $str_query="INSERT INTO equipment set equipment_id='$id',equipment_name='$name',manufacturer_name='$manufacturer', supplier_name='$supplier', laboratrory_id=$lab_id,safety_requirement='$safety_requirement'";
-	 return $this->query($str_query).mysql_error();
+	    function add_equipment($name,$id,$manufacturer,$supplier,$lab_id,$safety_requirement){
+	        $str_query="INSERT INTO equipment set equipment_id='$id',equipment_name='$name',manufacturer_name='$manufacturer', supplier_name='$supplier', laboratrory_id=$lab_id,safety_requirement='$safety_requirement'";
+	        return $this->query($str_query).mysql_error();
+	    }
+		/**
+	*@method boolean faults_log ($name,$id,$lab_id,$description,$date) loggs in equipment fault into se_inventory_faults table
+	*@param string $name name of equipment
+	*@param integer $id id of equipment
+	*@param integer $lab_id lab  where equipment belong
+	*@param string $description the fault occured
+	   **/
+	    function faults_log ($name,$id,$lab_id,$description,$date){
+		    $str_query="INSERT INTO se_inventory_faults set equipment_id=$id,equipment_name='$name',laboratrory_id=$lab_id,description='$description',date_of_damage=$date";
+		    return $this->query($str_query).mysql_error();
+	    }
 	}
 }
 ?>
