6b5f5ff742a971a28a1820dc1377cb5a5a0aa341
michaelannor <m.annor13@gmail.com>
2015-11-24 22:26:57 +0000

    checkin php fn - untested

--- project-group/controller/ajax-action.php
+++ project-group/controller/ajax-action.php
@@ -12,6 +12,10 @@
       check_out_equipment_cmd();
       break;
 
+    case 2:
+      check_in_equipment_cmd();
+      break;
+
     default:
       # code...
       break;
@@ -33,6 +37,30 @@
       if(verify_user_helper($user)){
         if($obj->check_out_equipment($user, $equipment, $borrowed, $to_return)){
           echo '{"result":1,"message": "checked out successfully"}';
+        }
+        else{
+          echo '{"result":0,"message": "transaction not added."}';
+        }
+      }
+    }
+  }
+
+  /**
+   * [[The check_in_equipment_cmd function is to add a transaction to the database after an equipment is logged back in, runs
+   * when called from the view]]
+   */
+  function check_in_equipment_cmd(){
+    include ("../model/transaction.php");
+    $obj = new transaction();
+    $user = $_REQUEST['user'];
+    $equipment = $_REQUEST['equipment'];
+    $borrowed = $_REQUEST['borrowed'];
+    $to_return = $_REQUEST['return'];
+
+    if(verify_equipment_helper($equipment)){
+      if(verify_user_helper($user)){
+        if($obj->check_in_equipment($equipment, $date_returned)){
+          echo '{"result":1,"message": "checked in successfully"}';
         }
         else{
           echo '{"result":0,"message": "transaction not added."}';
