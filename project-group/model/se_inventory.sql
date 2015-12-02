5b0d0ad22df0d5fde4895cd31910936873488d24
Nuralight <nuraabd4@gmail.com>
2015-12-02 03:44:30 +0000

    update

--- project-group/model/se_inventory.sql
+++ project-group/model/se_inventory.sql
@@ -86,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `se_inventory_users` (
   UNIQUE KEY `user_name` (`user_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 --
---Table structure for table `safety_requirement`
+--Table structure for table `se_inventory_faults`
 --
 CREATE TABLE IF NOT EXISTS `se_inventory_faults` (
   `equipment_id` int(10) NOT NULL PRIMARY KEY,
